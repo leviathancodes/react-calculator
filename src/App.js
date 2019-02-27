f80a9fa939596381e5ada24b8fa392f9ba2b0f20
lhuddlesto <lhuddlesto@gannett.com>
2019-02-27 08:43:35 -0500

    clear, positivie/negative, percentage, decimal and digit funcitionality and display

--- src/App.js
+++ src/App.js
@@ -13,13 +13,84 @@ class App extends Component {
     this.state = {
       displayValue: 0
     }
+    this.handleDigits = this.handleDigits.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handlePercentage = this.handlePercentage.bind(this);
+    this.handlePosiNeg = this.handlePosiNeg.bind(this);
   }
+
+  handleDigits(e) {
+    let val = this.state.displayValue;
+    let num = e.target.innerHTML
+    if (val.length >= 10) {
+      return
+    }
+
+    if (val == "ERR") {
+      return this.setState({displayValue: 0})
+    }
+
+    if ((val === "0" && num === "0") || (val == 0 && String(val).indexOf(".0") == -1) ) {
+      return this.setState({displayValue: num}) 
+    }
+
+    if (num === "." && val.includes(".")) {
+      return
+    }
+    
+    let newVal = val += num
+    console.log(newVal)
+    this.setState({displayValue: newVal})
+  }
+
+  handleClear(e) {
+    this.setState({displayValue: 0})
+  }
+
+  handlePercentage(e) {
+    
+    let val = this.state.displayValue;
+    let percentage = val/100;
+    percentage = percentage.toString();
+    
+    if (val == "ERR") {
+      return this.setState({displayValue: 0})
+    }
+
+    console.log(percentage);
+    
+    if (percentage.length >= 10) {
+      this.setState({displayValue: "ERR"})
+    }
+
+    if (percentage.includes('e') || percentage.length >= 10) {
+      return this.setState({displayValue: "ERR"})
+    }
+    
+    this.setState({displayValue: percentage})
+  }
+
+  handlePosiNeg(e) {
+
+    let val = this.state.displayValue;
+    if (val != 0 && val != "." && val != "ERR") {
+      this.setState({displayValue: "-" + val})
+    }
+    if (val[0] == "-") {
+      val = val.replace("-", "");
+      this.setState({displayValue: val})
+    }
+  }
+
 
   render() {
     return (
       <div className="container">
         <Display displayValue={this.state.displayValue}/>
-        <Buttons />
+        <Buttons clickNum={this.handleDigits} 
+        clear={this.handleClear} 
+        percentage={this.handlePercentage}
+        posiNeg={this.handlePosiNeg}/>
       </div>
     );
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,37 +1,72 @@
 import React from 'react';
 
 const Buttons = props => {
+
+const buttonObj = [
+    
+    {
+        button: "button",
+        row1: {
+            class: "button-row-1",
+            id: ["clear", "", "", "divide"],
+            key: ["C", "+/-", "%", "/"]
+        },
+        row2: {
+            class: "button-row-2",
+            id: ["seven", "eight", "nine", "multiply"],
+            key: ["7", "8", "9", "X"]
+        },
+        row3: {
+            class: "button-row-3",
+            id: ["four", "five", "six", "subtract"],
+            key: ["4", "5", "6", "-"]
+        },
+        row4: {
+            class: "button-row-4",
+            id: ["one", "two", "three", "add"],
+            key: ["1", "2", "3", "+"]
+        },
+        row5: {
+            class: "button-row-5",
+            id: ["zero", "decimal", "equals"],
+            keys: ["0", ".", "="]
+        }
+    },
+]
+
+
+
     return (
-<div class="buttons">
-    <div class="button-row button-row-1">
-   <div class="button" id="clear">C</div>
-      <div class="button">+/-</div>
-      <div class="button">%</div>
-      <div class="button button-side" id="divide">/</div>   
+<div className="buttons">
+    <div className="button-row button-row-1">
+   <div className="button" onClick={props.clear} id="clear">C</div>
+      <div className="button" onClick={props.posiNeg}>+/-</div>
+      <div className="button" onClick={props.percentage}>%</div>
+      <div className="button button-side" id="divide">/</div>   
     </div>
-    <div class="button-row button-row-2">
-      <div class="button" id="seven">7</div>
-      <div class="button" id="eight">8</div>
-      <div class="button" id="nine">9</div>
-      <div class="button button-side" id="multiply">X</div>
+    <div className="button-row button-row-2">
+      <div className="button" onClick={props.clickNum} id="seven">7</div>
+      <div className="button" onClick={props.clickNum} id="eight">8</div>
+      <div className="button" onClick={props.clickNum} id="nine">9</div>
+      <div className="button button-side" id="multiply">X</div>
     </div>
-    <div class="button-row button-row-3">
-      <div class="button" id="four">4</div>
-      <div class="button" id="five">5</div>
-      <div class="button" id="six">6</div>
-      <div class="button button-side" id="subtract">-</div>
+    <div className="button-row button-row-3">
+      <div className="button" onClick={props.clickNum} id="four">4</div>
+      <div className="button" onClick={props.clickNum} id="five">5</div>
+      <div className="button" onClick={props.clickNum} id="six">6</div>
+      <div className="button button-side" id="subtract">-</div>
       
     </div>
-    <div class="button-row button-row-4">
-      <div class="button" id="three">3</div>
-      <div class="button" id="two">2</div>
-      <div class="button" id="one">1</div>
-      <div class="button button-side" id="add">+</div>
+    <div className="button-row button-row-4">
+      <div className="button" onClick={props.clickNum} id="three">3</div>
+      <div className="button" onClick={props.clickNum} id="two">2</div>
+      <div className="button" onClick={props.clickNum} id="one">1</div>
+      <div className="button button-side" id="add">+</div>
     </div>
-    <div class="button-row button-row-5" id='specificity-hack'>
-      <div class="button" id="zero">0</div>
-      <div class="button" id="decimal">.</div>
-      <div class="button button-side" id="equals">=</div>
+    <div className="button-row button-row-5">
+      <div className="button" onClick={props.clickNum} id="zero">0</div>
+      <div className="button" onClick={props.clickNum} id="decimal">.</div>
+      <div className="button button-side" id="equals">=</div>
     </div>
   </div>
 
