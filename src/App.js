c2bd2dfbad3ededaae8830ad5ece7dc0c4cc828c
lhuddlesto <lhuddlesto@gannett.com>
2019-03-01 21:14:54 -0500

    operator functionality

--- src/App.js
+++ src/App.js
@@ -5,46 +5,94 @@ import Display from './components/Display';
 import Buttons from './components/Buttons';
 
 
+
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      displayValue: 0
+      displayValue: 0,
+      calculation: ["0"],
+      lastInput: "init",
+      evaluated: false
     }
     this.handleDigits = this.handleDigits.bind(this);
+    this.handleDecimal = this.handleDecimal.bind(this);
     this.handleClear = this.handleClear.bind(this);
     this.handlePercentage = this.handlePercentage.bind(this);
     this.handlePosiNeg = this.handlePosiNeg.bind(this);
+    this.handleCalculation = this.handleCalculation.bind(this);
+    this.handleEquals = this.handleEquals.bind(this)
   }
 
   handleDigits(e) {
     let val = this.state.displayValue;
-    let num = e.target.innerHTML
+    let num = e.target.innerHTML;
+    let lastInput = this.state.lastInput;
+    let calculationState = this.state.calculation;
+
+    if (lastInput == "init" ) {
+      val = num
+      return this.setState({displayValue: num, lastInput: num})
+    }
+    
     if (val.length >= 10) {
-      return
+      return 
     }
 
     if (val == "ERR") {
       return this.setState({displayValue: 0})
     }
-
-    if ((val === "0" && num === "0") || (val == 0 && String(val).indexOf(".0") == -1) ) {
-      return this.setState({displayValue: num}) 
-    }
-
-    if (num === "." && val.includes(".")) {
+    if (val == "0" && num == "0") {
       return
     }
+
+    if (num != "0" && val == "0") {
+      val = num;
+      return this.setState({displayValue: num, lastInput: num})
+    }
+
+    if ( 
+      (lastInput == "X" ||
+       lastInput == "+" ||
+       lastInput == "-" ||
+       lastInput == "÷")) {
+        return this.setState({displayValue: num, lastInput: num})
+      }
+
+    let newVal = val + num
+    this.setState({displayValue: newVal, 
+      lastInput: num})
+    }
+
+
+
+
+  handleDecimal(e) {
+    let val = this.state.displayValue;
+    let decimal = e.target.innerHTML;
+    let lastInput = this.state.lastInput;
+
+    if (val[val.length - 1] == decimal) {
+      return
+    }
+
+    if (val.includes(".")) {
+      return
+    }
+
+  
+    val += decimal
+    this.setState({displayValue: val, lastInput: decimal})
+
     
-    let newVal = val += num
-    console.log(newVal)
-    this.setState({displayValue: newVal})
   }
 
   handleClear(e) {
     this.setState({displayValue: 0})
+    this.setState({calculation: ["0"]})
   }
 
   handlePercentage(e) {
@@ -82,15 +130,65 @@ class App extends Component {
     }
   }
 
+  handleCalculation(e) {
+    let val = this.state.displayValue;
+    let operator = e.target.innerHTML;
+    let calculationState = this.state.calculation.slice();
+    let lastInput = e.target.innerHTML;
+    
+
+
+    if (operator == "X") {
+      operator = "*"
+    } 
+    if (operator == "÷") {
+      operator = "/"
+    }
+
+    if (this.state.lastInput === "X" || 
+    this.state.lastInput === "+" ||
+    this.state.lastInput === "-" ||
+    this.state.lastInput === "÷") {
+    calculationState.splice(calculationState.length - 1, 1)
+    calculationState.push(operator)
+    this.setState({calculation: calculationState, lastInput: lastInput})
+    return console.log(calculationState, lastInput, this.state.lastInput)
+    }
+
+    calculationState.push(val)
+    calculationState.push(operator)
+
+    console.log(calculationState, lastInput, this.state.lastInput)
+    this.setState({calculation: calculationState, lastInput: lastInput})
+  }
+
+  handleEquals(e) {
+    let val = this.state.displayValue;
+    let calculationState = this.state.calculation.slice();
+    calculationState.shift()
+    calculationState.push(String(val))
+    let solution = (eval(calculationState.join(" ")));
+    if (String(solution).length >= 10) {
+      solution = solution.toFixed(6);
+    }
+    this.setState({displayValue: solution, calculation: ["0"], lastInput: "="})
+    console.log(this.state.calculation)
+  }
+
+
 
   render() {
     return (
       <div className="container">
         <Display displayValue={this.state.displayValue}/>
         <Buttons clickNum={this.handleDigits} 
+        decimal={this.handleDecimal}
         clear={this.handleClear} 
         percentage={this.handlePercentage}
-        posiNeg={this.handlePosiNeg}/>
+        posiNeg={this.handlePosiNeg}
+        addition={this.handlePlus}
+        calculation={this.handleCalculation}
+        equals={this.handleEquals}/>
       </div>
     );
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,71 +2,37 @@ import React from 'react';
 
 const Buttons = props => {
 
-const buttonObj = [
-    
-    {
-        button: "button",
-        row1: {
-            class: "button-row-1",
-            id: ["clear", "", "", "divide"],
-            key: ["C", "+/-", "%", "/"]
-        },
-        row2: {
-            class: "button-row-2",
-            id: ["seven", "eight", "nine", "multiply"],
-            key: ["7", "8", "9", "X"]
-        },
-        row3: {
-            class: "button-row-3",
-            id: ["four", "five", "six", "subtract"],
-            key: ["4", "5", "6", "-"]
-        },
-        row4: {
-            class: "button-row-4",
-            id: ["one", "two", "three", "add"],
-            key: ["1", "2", "3", "+"]
-        },
-        row5: {
-            class: "button-row-5",
-            id: ["zero", "decimal", "equals"],
-            keys: ["0", ".", "="]
-        }
-    },
-]
-
-
-
     return (
 <div className="buttons">
     <div className="button-row button-row-1">
    <div className="button" onClick={props.clear} id="clear">C</div>
       <div className="button" onClick={props.posiNeg}>+/-</div>
       <div className="button" onClick={props.percentage}>%</div>
-      <div className="button button-side" id="divide">/</div>   
+      <div className="button button-side" onClick={props.calculation}id="divide">÷</div>   
     </div>
     <div className="button-row button-row-2">
       <div className="button" onClick={props.clickNum} id="seven">7</div>
       <div className="button" onClick={props.clickNum} id="eight">8</div>
       <div className="button" onClick={props.clickNum} id="nine">9</div>
-      <div className="button button-side" id="multiply">X</div>
+      <div className="button button-side" onClick={props.calculation} id="multiply">X</div>
     </div>
     <div className="button-row button-row-3">
       <div className="button" onClick={props.clickNum} id="four">4</div>
       <div className="button" onClick={props.clickNum} id="five">5</div>
       <div className="button" onClick={props.clickNum} id="six">6</div>
-      <div className="button button-side" id="subtract">-</div>
+      <div className="button button-side" onClick={props.calculation} id="subtract">-</div>
       
     </div>
     <div className="button-row button-row-4">
       <div className="button" onClick={props.clickNum} id="three">3</div>
       <div className="button" onClick={props.clickNum} id="two">2</div>
       <div className="button" onClick={props.clickNum} id="one">1</div>
-      <div className="button button-side" id="add">+</div>
+      <div className="button button-side" onClick={props.calculation}id="add">+</div>
     </div>
     <div className="button-row button-row-5">
       <div className="button" onClick={props.clickNum} id="zero">0</div>
-      <div className="button" onClick={props.clickNum} id="decimal">.</div>
-      <div className="button button-side" id="equals">=</div>
+      <div className="button" onClick={props.decimal} id="decimal">.</div>
+      <div className="button button-side" onClick={props.equals} id="equals">=</div>
     </div>
   </div>
 
